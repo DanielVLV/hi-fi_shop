4ef4fbaaa76cfc81237842d8b59d63026d68d86c
DanielVLV <odindlrus@gmail.com>
2023-03-21 17:18:57 +0400

    card products

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,10 +2,24 @@ const React = require('react');
 
 const Layout = require('./Layout');
 
-module.exports = function Home() {
+const User = require('./User');
+
+module.exports = function Home({ user, allProducts }) {
   return (
-    <Layout>
-      <h1>Home</h1>
+    <Layout user={user}>
+      <div>
+      </div>
+      <div className="allProducts">
+        {allProducts
+          ? (
+            <>
+              {allProducts.map((el) => (
+                <Product el={el} />
+              ))}
+            </>
+          ) : (null)}
+        <User />
+      </div>
     </Layout>
   );
 };
