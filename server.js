0fc3b8facaaadccf79a1c2ddc2bc37df4724dc74
Kirill <neroo9618@gmail.com>
2023-03-22 09:13:51 +0300

    profile_page

--- server.js
+++ server.js
@@ -7,48 +7,55 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 
-const login = require('./src/routes/login.router');
-const registration = require('./src/routes/registration.router');
-const logout = require('./src/routes/logout');
+const renderTemplate = require('./src/renderTemplate');
+const Profile = require('./src/views/Profile');
+
+// const login = require('./src/routes/login.router');
+// const registration = require('./src/routes/registration.router');
+// const logout = require('./src/routes/logout');
 
 const app = express();
-const { PORT, COOKIE_SEKRET } = process.env;
+// const { PORT, COOKIE_SEKRET } = process.env;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const sessionConfig = {
-  name: 'Cookie',
-  store: new FileStore(),
-  secret: COOKIE_SEKRET ?? '123',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 10,
-    httpOnly: true,
-  },
-};
+// const sessionConfig = {
+//   name: 'Cookie',
+//   store: new FileStore(),
+//   secret: COOKIE_SEKRET ?? '123',
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     maxAge: 1000 * 60 * 60 * 24 * 10,
+//     httpOnly: true,
+//   },
+// };
 
-app.use(session(sessionConfig));
-app.use(express.static(path.resolve('public')));
+// app.use(session(sessionConfig));
+// app.use(express.static(path.resolve('public')));
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(process.cwd(), 'public')));
 
-app.get('/login', login);
-app.get('/logout', logout);
-app.get('/registration', registration);
-app.get('/login', login);
+// app.get('/login', login);
+// app.get('/logout', logout);
+// app.get('/registration', registration);
+// app.get('/login', login);
 
-app.post('/registration', registration);
-app.post('/login', login);
+// app.post('/registration', registration);
+// app.post('/login', login);
 
-const indexRouter = require('./src/routes/home.router');
+// const indexRouter = require('./src/routes/home.router');
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
+
+app.get('/', (req, res) => {
+  renderTemplate(Profile, null, res);
+});
 
 app.get('*', (req, res) => { res.redirect('/'); });
 
-app.listen(PORT, () => {
-  console.log(`Сервер запущен порт: ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Сервер запущен порт: ${3000}`);
 });
