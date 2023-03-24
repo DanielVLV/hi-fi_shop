1aa9474f04ea64f704cc97e4df78054ee5a22e2e
DanielVLV <odindlrus@gmail.com>
2023-03-24 13:57:54 +0400

    swiper main

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,11 +3,13 @@ const React = require('react');
 const Layout = require('./Layout');
 
 const ProductCard = require('./ProductCard');
+const SwiperHomePage = require('./swiperHomePage');
 
 module.exports = function Home({ user, allProductCard }) {
   return (
     <Layout user={user}>
       <div className="container">
+      <SwiperHomePage />
         <a href="/products/1">Product 1 page</a>
         <div style={{ marginTop: '150px', color: 'white' }}>
           {allProductCard ? (
